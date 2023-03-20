cfbdb81623c071739d5a69cd07a43d76181d7cf9
Tyrn <a@b.com>
2023-03-21 00:08:19 +0300

    platform.dart reformatted

--- lib/platform.dart
+++ lib/platform.dart
@@ -5,21 +5,27 @@ class Platform {
   static bool get isDesktop {
     return isLinux || isMacOS || isWindows;
   }
+
   static bool get isLinux {
     return defaultTargetPlatform == TargetPlatform.linux;
   }
+
   static bool get isMacOS {
     return defaultTargetPlatform == TargetPlatform.macOS;
   }
+
   static bool get isWindows {
     return defaultTargetPlatform == TargetPlatform.windows;
   }
+
   static bool get isFuchsia {
     return defaultTargetPlatform == TargetPlatform.fuchsia;
   }
+
   static bool get isAndroid {
     return defaultTargetPlatform == TargetPlatform.android;
   }
+
   static bool get isIOS {
     return defaultTargetPlatform == TargetPlatform.iOS;
   }
