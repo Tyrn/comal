6ba6da9581f156fc091472e01d2e97736079d9c3
Tyrn <a@b.com>
2023-04-02 11:59:18 +0300

    counter_bloc_test.dart created

---
+++ test/counter/bloc/counter_bloc_test.dart
@@ -0,0 +1,28 @@
+// Package imports:
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+// Project imports:
+import 'package:comal/counter/counter.dart';
+
+void main() {
+  group('CounterBloc', () {
+    test('initial state is 0', () {
+      expect(CounterBloc().state, equals(0));
+    });
+
+    blocTest<CounterCubit, int>(
+      'emits [1] when increment is called',
+      build: CounterCubit.new,
+      act: (cubit) => cubit.increment(),
+      expect: () => [equals(1)],
+    );
+
+    blocTest<CounterCubit, int>(
+      'emits [-1] when decrement is called',
+      build: CounterCubit.new,
+      act: (cubit) => cubit.decrement(),
+      expect: () => [equals(-1)],
+    );
+  });
+}
