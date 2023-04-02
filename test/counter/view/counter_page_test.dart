8a1ab310fd7465d8198622edd0d8f5739ec50a42
Tyrn <a@b.com>
2023-04-02 12:21:14 +0300

    Cubit removed completely

--- test/counter/view/counter_page_test.dart
+++ test/counter/view/counter_page_test.dart
@@ -11,8 +11,6 @@ import 'package:mocktail/mocktail.dart';
 import 'package:comal/counter/counter.dart';
 import '../../helpers/helpers.dart';
 
-// class MockCounterCubit extends MockCubit<int> implements CounterCubit {}
-
 class MockCounterBloc extends MockBloc<CounterEvent, int>
     implements CounterBloc {}
 
@@ -25,21 +23,17 @@ void main() {
   });
 
   group('CounterView', () {
-    // late CounterCubit counterCubit;
     late CounterBloc counterBloc;
 
     setUp(() {
-      // counterCubit = MockCounterCubit();
       counterBloc = MockCounterBloc();
     });
 
     testWidgets('renders current count', (tester) async {
       const state = 42;
-      // when(() => counterCubit.state).thenReturn(state);
       when(() => counterBloc.state).thenReturn(state);
       await tester.pumpApp(
         BlocProvider.value(
-          // value: counterCubit,
           value: counterBloc,
           child: const CounterView(),
         ),
@@ -49,37 +43,29 @@ void main() {
 
     testWidgets('calls increment when increment button is tapped',
         (tester) async {
-      // when(() => counterCubit.state).thenReturn(0);
       when(() => counterBloc.state).thenReturn(0);
-      // when(() => counterCubit.increment()).thenReturn(null);
       when(() => counterBloc.add(CounterIncrementPressed())).thenReturn(null);
       await tester.pumpApp(
         BlocProvider.value(
-          // value: counterCubit,
           value: counterBloc,
           child: const CounterView(),
         ),
       );
       await tester.tap(find.byIcon(Icons.add));
-      // verify(() => counterCubit.increment()).called(1);
       verifyNever(() => counterBloc.add(CounterIncrementPressed()));
     });
 
     testWidgets('calls decrement when decrement button is tapped',
         (tester) async {
-      // when(() => counterCubit.state).thenReturn(0);
       when(() => counterBloc.state).thenReturn(0);
-      // when(() => counterCubit.decrement()).thenReturn(null);
       when(() => counterBloc.add(CounterDecrementPressed())).thenReturn(null);
       await tester.pumpApp(
         BlocProvider.value(
-          // value: counterCubit,
           value: counterBloc,
           child: const CounterView(),
         ),
       );
       await tester.tap(find.byIcon(Icons.remove));
-      // verify(() => counterCubit.decrement()).called(1);
       verifyNever(() => counterBloc.add(CounterDecrementPressed()));
     });
   });
