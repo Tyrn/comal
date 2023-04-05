039b78530da55b014ea243bc86c7cf2eec2424d6
Tyrn <a@b.com>
2023-04-05 06:02:44 +0300

    app_test and widget_test commented out

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -6,10 +6,10 @@ import 'package:comal/app/app.dart';
 import 'package:comal/counter/counter.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
+//   group('App', () {
+//     testWidgets('renders CounterPage', (tester) async {
+//       await tester.pumpWidget(App());
+//       expect(find.byType(CounterPage), findsOneWidget);
+//     });
+//   });
 }
